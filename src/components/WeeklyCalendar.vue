5eea34bceb3e2a4d3ebb83139cb800de4e45f948
Hironosuke Watanabe <15s147139watanabe@gmail.com>
2021-11-28 22:11:42 +0900

    set locale to japan

--- src/components/WeeklyCalendar.vue
+++ src/components/WeeklyCalendar.vue
@@ -16,6 +16,7 @@ export default {
   data () {
     return {
       calendarOptions: {
+        locale: 'ja',
         plugins: [ timeGridPlugin, interactionPlugin ],
         initialView: 'timeGridWeek',
         selectable: true,
