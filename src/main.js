665611cace34069acb8478b806d0a2ba09a8751a
Hironosuke Watanabe <15s147139watanabe@gmail.com>
2021-12-15 23:39:42 +0900

    add settings for debugging

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.config.devtools = true
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
