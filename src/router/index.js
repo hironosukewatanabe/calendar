555ea3d815e7f63ec83b5adf34ae6c25dec872bb
Hironosuke Watanabe <15s147139watanabe@gmail.com>
2022-01-10 23:21:27 +0900

    add a route to login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import WeeklyCalendar from '@/components/WeeklyCalendar'
+import SignIn from '@/components/SignIn'
+import SignUp from '@/components/SignUp'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignIn
+    },
     {
       path: '/',
       name: 'WeeklyCalendar',
